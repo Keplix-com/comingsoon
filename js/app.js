f6e48db983683689e5ebf4468faddf11c245d923
helyx <46862270+Adi202001@users.noreply.github.com>
2023-07-05 01:13:13 +0530

    first-push

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCk3ENWonN0oFysPGNhpqCaZqjj_Lt8ocM",
+    authDomain: "keplix-b081e.firebaseapp.com",
+    databaseURL: "https://keplix-b081e-default-rtdb.firebaseio.com",
+    projectId: "keplix-b081e",
+    storageBucket: "keplix-b081e.appspot.com",
+    messagingSenderId: "717741577031",
+    appId: "1:717741577031:web:115d03c5c941d7a6bc7262",
+    measurementId: "G-4CC70WVJ1J"
+};
+
+firebase.initialzeApp(firebaseConfig);
